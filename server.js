84c6f9a4a1a3652bba09497863842d688193bbc1
Sabbir-XD <hossain.sabbir.dev@gmail.com>
2026-04-04 05:06:39 +0600

    update it api

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ app.use(express.json());
 // Routes
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 app.use("/api/orders", orderRoutes);
 
 // DB connection
